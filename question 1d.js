d1a6f3a7a9cda63325623d056474541a395581d3
Rayed Khan <60972839+racray@users.noreply.github.com>
2021-09-28 19:29:01 +0530

    Update question 1d.js

--- question 1d.js
+++ question 1d.js
@@ -1,16 +1,11 @@
-//////////// REST COUNTRY UPI NOT OPENING////////////////
-
 const getC =() =>{
     const xhr = new XMLHttpRequest;
-    xhr.open("GET","https://restcountries.eu/rest/v2/all");
+    xhr.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
     xhr.responseType ="json";
     xhr.onload=()=>{
         const countriesList = xhr.response;
         const totalpopulation = countriesList.reduce((totalpop, countrypop)=>totalpop+countrypop.population,0);
         console.log(totalpopulation);
-        // #######      OUTPUT Failed to load resource: the server responded with a status of 404 (Not Found)
-
-
     }
     xhr.send();
 }
