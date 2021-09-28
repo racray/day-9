49c05c872c8066783176c2634f4082e9683a8e9f
Rayed Khan <60972839+racray@users.noreply.github.com>
2021-09-28 19:28:37 +0530

    Update question 1c.js

--- question 1c.js
+++ question 1c.js
@@ -1,8 +1,6 @@
-//////////// REST COUNTRY UPI NOT OPENING////////////////
-
 const getC =() =>{
     const xhr = new XMLHttpRequest;
-    xhr.open("GET","https://restcountries.eu/rest/v2/all");
+    xhr.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
     xhr.responseType ="json";
     xhr.onload=()=>{
         const countriesList = xhr.response;
